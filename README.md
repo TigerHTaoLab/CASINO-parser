637b0dab7cb757ab6c15131b3e5d9d49e66eae2b
czhou <chengzhe.zhou@foxmail.com>
2021-06-14 09:42:11 +0800

    cmd argument parser

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Parser for CASINO V2
 
 This is a parser for the raw data output from CASINO v2.
+
+## Compile
+
+```shell
+mkdir bin -p
+mkdir build -p
+cd build
+cmake ..
+make -j
+```
+## Usage
+
+
--- examples/main.cpp
+++ examples/main.cpp
@@ -1,30 +1,97 @@
 #include <io.hpp>
 #include <histogram.hpp>
+#include <vector>
 #include <iostream>
+bool ProgramOptionExists(int argc, char **argv, const std::string &option)
+{
+    return std::find(argv, argv + argc, option) != argv + argc;
+}
+std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems)
+{
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, delim))
+    {
+        elems.push_back(item);
+    }
+    return elems;
+}
+std::vector<std::string> GetProgramOptionAsString(
+    int argc,
+    char **argv,
+    const std::string &option,
+    char delim,
+    const std::string &default_value = "")
+{
+    char **itr = std::find(argv, argv + argc, option);
+    std::vector<std::string> optStrVec;
+    if (itr != argv + argc && ++itr != argv + argc)
+    {
 
-int main()
+        split(std::string(*itr), delim, optStrVec);
+    }
+
+    return optStrVec;
+}
+
+int main(int argc, char *argv[])
 {
 
-    std::string datFile = "/home/be/Projects/CASINO_Visualizer/data/200k/05/00.dat";
-    std::string matFile_w = "./test.mat";
-    std::string rangeFile_w = "./test.range";
-    std::string histogramFile_w = "./test.csv";
+    std::vector<std::string> cmdArgs(argv, argv + argc);
 
-    //casino_parser::IO::openDAT(datFile, matFile_w, rangeFile_w);
+    if (argc < 3)
+    {
+        std::cerr << "Usage: ./dat2hist path/to/file/xx.dat  path/to/directory/output" << std::endl;
+        return 1;
+    }
+    if (ProgramOptionExists(argc, argv, "-h") || ProgramOptionExists(argc, argv, "--help"))
+    {
+        std::cout << "-d2m       : convert from xx.dat to path/to/directory/mat.txt\n";
+        std::cout << "-m2h a_b_c : convert from path/to/directory/mat.txt path/to/directory/histogram-z.csv\n";
+        std::cout << "             where x = zMin, y = zMax, c = number of bins\n";
+        return 1;
+    }
 
-    Eigen::MatrixXd datMat;
-    casino_parser::IO::openMAT(matFile_w, datMat);
+    std::string datFile = argv[1];
+    std::string outputDir = argv[2];
+    std::string matFile_w = outputDir + "/mat.txt";
+    std::string rangeFile_w = outputDir + "/range.txt";
+    std::string histogramFile_w = outputDir + "/histogram-z.csv";
 
-    Eigen::MatrixXd dim;
-    dim.resize(3, 1);
-    dim(0, 0) = 0;
-    dim(1, 0) = 800;
-    dim(2, 0) = 800;
+    if (ProgramOptionExists(argc, argv, "-d2m"))
+    {
+        std::cout << "Reading from " << datFile << "...\n";
+        std::cout << "Writing to " << matFile_w << "...\n";
+        casino_parser::IO::openDAT(datFile, matFile_w, rangeFile_w);
+        std::cout << "Done!\n";
+    }
 
-    casino_parser::HistogramZ<double> histogramZ;
+    if (ProgramOptionExists(argc, argv, "-m2h"))
+    {
 
-    casino_parser::parseTrajectories<double>(datMat, dim, histogramZ, 0);
-    casino_parser::IO::writeToCSV(histogramFile_w, histogramZ.rawMatrix);
+        auto optStrVec = GetProgramOptionAsString(argc, argv, "-m2h", '_');
+
+        Eigen::MatrixXd dim;
+        dim.resize(3, 1);
+        dim(0, 0) = stod(optStrVec[0]);
+        dim(1, 0) = stod(optStrVec[1]);
+        dim(2, 0) = stod(optStrVec[2]);
+
+        std::cout << "Histogram range: zMin = " << dim(0, 0) << " , zMax= " << dim(0, 0) << " , bin #s = " << dim(2, 0) << "\n";
+
+        std::cout << "Reading from " << matFile_w << "...\n";
+        Eigen::MatrixXd datMat;
+        casino_parser::IO::openMAT(matFile_w, datMat);
+        std::cout << "Done!\n";
+
+        casino_parser::HistogramZ<double> histogramZ;
+        std::cout << "Calculating histogram for all trajectories...\n";
+        casino_parser::parseTrajectories<double>(datMat, dim, histogramZ, 0);
+        std::cout << "Done!\n";
+        std::cout << "Writing to " << histogramFile_w << "...\n";
+        casino_parser::IO::writeToCSV(histogramFile_w, histogramZ.rawMatrix);
+        std::cout << "Done!\n";
+    }
 
     return 0;
 }
